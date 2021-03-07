4b0dfab0adec235f221309d11e4016c22eb2b2f9
komaitaira <komaitaira@gmail.com>
2021-03-07 14:38:33 +0900

    Add state

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: "Hello",
+      counter: 0,
+      flg: true,
+    };
+    this.doAction = this.doAction.bind(this);
 
+    // let timer = setInterval(() => {
+    //   this.setState({
+    //     count: this.state.count + 1,
+    //     msg: `count: ${this.state.count}`
+    //   });
+    // }, 1000);
+  }
+
+  doAction(event) {
+    this.setState({
+      counter: this.state.counter + 1,
+      msg: `*** count: ${this.state.counter} ***`,
+      flg: !this.state.flg,
+    });
+  };
+
+  render() {
+    return <div>
+      <h1 className="bg-primary text-white display-4">React</h1>
+      <div className="container">
+        <p className="subtitle">Count Number</p>
+        {this.state.flg ?
+          <div className="alert alert-primary text-center">
+            <p className="h5 mb-5">{this.state.msg}</p>
+            <button className="btn btn-primary" onClick={this.doAction}>Click</button>
+          </div>
+          :
+          <div className="alert alert-danger text-center">
+            <p className="h5 mb-5">{this.state.msg}</p>
+            <button className="btn btn-primary" onClick={this.doAction}>Click</button>
+          </div>
+        }
+      </div>
+    </div>
+  }
+}
 export default App;
