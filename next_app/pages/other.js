def1b87c328bc8167d0e8b41e7836174ecb3553d
komaitaira <komaitaira@gmail.com>
2021-03-24 23:14:01 +0900

    Add other.js 複数ファイルを作成する

--- next_app/pages/other.js
+++ next_app/pages/other.js
@@ -1,8 +1,9 @@
 import Header from './header'
 import Link from 'next/link'
 
-export default function Home() {
-  const title = "Index";
+export default function Other() {
+  const title = "Other"
+
   return (
     <div>
       <Header title={title}/>
@@ -10,9 +11,9 @@ export default function Home() {
       <div className="container">
         <h4 className="my-3　text-primary text-center">{title}</h4>
         <div className="card p-3 text-center">
-          <p className="h5">これはページ移動のサンプルです</p>
-          <Link href="/other">
-            <a>Go to Other page &gt;&gt;</a>
+          <p className="h5">これはもう一つのページです</p>
+          <Link href="/">
+            <a>&lt;&lt; Back to Index page</a>
           </Link>
         </div>
       </div>
