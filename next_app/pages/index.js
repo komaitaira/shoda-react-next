d69673410faf256063a7044037cd38236a4fda63
komaitaira <komaitaira@gmail.com>
2021-03-24 23:54:49 +0900

    Add MyImage.js

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,4 +1,5 @@
 import Layout from '../components/layout'
+import MyImage from '../components/image'
 import Link from 'next/link'
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
       <Layout header="Next.js" title="Top page">
         <div className="card p-3 text-center">
           <h4 className="my-3　text-primary text-center">Welcome to Next.js!</h4>
+          <MyImage fname="image.jpg" size="300"/>
           <Link href="./other">
             <button className="btn btn-primary">Go to Other page &gt;&gt;</button>
           </Link>
