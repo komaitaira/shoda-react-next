4b8df789e98e4267e24868fee8e8d157a232e3ed
komaitaira <komaitaira@gmail.com>
2021-03-28 17:28:52 +0900

    Add hello apiにidパラメータを追加

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,30 +1,21 @@
-// import {useState} from 'react'
+import {useState} from 'react'
 import Layout from '../components/layout'
 import useSWR from 'swr'
 
 export default function Home() {
-  const {data} = useSWR('/data.json')
+  const [address, setAddress] = useState('/api/hello');
+  const { data, err } = useSWR(address);
 
+  const onChange = (e) => {
+    setAddress('/api/hello?id=' + e.target.value)
+  }
   
   return (
     <div>
       <Layout header="Next.js" title="Top page">
         <div className="alert alert-primary text-center">
-          <h5 className="mb-4">{data !== undefined ? data.message : 'error...'}</h5>
-          <table className="table table-dark">
-            <thead className="">
-              <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
-            </thead>
-            <tbody>
-              {data !== undefined ? data.data.map((value, key) => (
-                <tr key={key}>
-                  <th>{value.name}</th>
-                  <td>{value.mail}</td>
-                  <td>{value.age}</td>
-                </tr>
-              )) : <tr><th></th><td>No data</td><td></td></tr>}
-            </tbody>
-          </table>
+          <h5 className="mb-4">{JSON.stringify(data)}</h5>
+          <input type="number" className="form-control" onChange={onChange}/>
         </div>
       </Layout>
     </div>
